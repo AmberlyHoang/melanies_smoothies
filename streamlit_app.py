1e7ef8a2aa9c5c5fad042430c547a6a275021ae5
AmberlyHoang <84863281+AmberlyHoang@users.noreply.github.com>
2025-02-05 12:26:31 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,8 +32,8 @@ st.stop()
 
 # Convert the snowpark dataframe to a pandas dataframe so we can use the LOC function
 pd_df = my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+#st.dataframe(pd_df)
+#st.stop()
 
 # https://docs.streamlit.io/develop/api-reference/widgets/st.multiselect
 ingredients_list = st.multiselect(
@@ -57,6 +57,10 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         # += operator means "add this to what is already in the variable" so each time the FOR Loop is repeated, a new fruit name is appended to the existing string. 
         ingredients_string += fruit_chosen + ' ' 
+
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+
         st.subheader(fruit_chosen + ' Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
         #st.text(smoothiefroot_response.json())
